dfb732f819bcbeb1a01395adc019a2a57ceb99bf
Sara Carbaugh <sara@saracarbaugh.com>
2016-11-07 14:11:50 -0500

    ie logos

--- assets/themes/themes/pattaconk/lib/js/min/homepage-jquery-min.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function($){var e=$("#homepage-widgets section.widget").length;$("#homepage-widgets").addClass("cols-"+e);var d=12/e;$("#homepage-widgets section.widget").addClass("col-sm-"+d),$("#homepage-widgets section.widget").addClass("col-xs-12"),$(window).width()>480});
